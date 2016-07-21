bd3517f885e709a4a84a8496eb02f03babdd20b8
CarolineBenn <lineybenn@gmail.com>
2016-07-21 14:40:16 +0100

    Added image column to dresses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721132657) do
+ActiveRecord::Schema.define(version: 20160721133940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160721132657) do
     t.integer  "rrp"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "orders", force: :cascade do |t|
